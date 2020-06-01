f7cbfa6cfccef19196aaeac4bffa96cb9730cd8e
yuebaiXu <xyb1114@gmail.com>
2020-06-01 18:27:52 +0200

    Updates

---
+++ precache-manifest.cdab5898b9171b432f6000fddf96820f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff8a3beb3c4e1fcbeedcb3d6ee0fb26",
+    "url": "/hk-film-visualization/index.html"
+  },
+  {
+    "revision": "064f99b7d6418137c4e0",
+    "url": "/hk-film-visualization/static/css/main.6ed91520.chunk.css"
+  },
+  {
+    "revision": "bcb6ced64faf9b6f8330",
+    "url": "/hk-film-visualization/static/js/2.3dc80d11.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/hk-film-visualization/static/js/2.3dc80d11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064f99b7d6418137c4e0",
+    "url": "/hk-film-visualization/static/js/main.4f268de8.chunk.js"
+  },
+  {
+    "revision": "9d43ce256a149990b616",
+    "url": "/hk-film-visualization/static/js/runtime-main.6ec66db4.js"
+  },
+  {
+    "revision": "d34ff4c62e70da968bb1281f8da783bc",
+    "url": "/hk-film-visualization/static/media/hkfont.d34ff4c6.ttc"
+  }
+]);
